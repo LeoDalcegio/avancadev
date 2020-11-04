10291913166eebacfe87ff60c268856225f203f0
Leonardo Luís Dalcegio <leoodalcegio@hotmail.com>
2020-11-04 16:46:51 +0000

    Created microservice "d" for challenge one

--- microsservicos-aula-1/c/c.go
+++ microsservicos-aula-1/c/c.go
@@ -3,8 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+
+	"github.com/hashicorp/go-retryablehttp"
 )
 
 type Coupon struct {
@@ -47,11 +51,45 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	result := Result{Status: valid}
 
+	if result.Status == "invalid" {
+		resultCoupon := makeHttpCall("http://localhost:9093", coupon)
+		result.Status = resultCoupon.Status
+	}
+
 	jsonResult, err := json.Marshal(result)
 	if err != nil {
 		log.Fatal("Error converting json")
 	}
 
 	fmt.Fprintf(w, string(jsonResult))
+
+}
+
+func makeHttpCall(urlMicroservice string, coupon string) Result {
+
+	values := url.Values{}
+	values.Add("coupon", coupon)
+
+	retryClient := retryablehttp.NewClient()
+	retryClient.RetryMax = 5
+
+	res, err := retryClient.PostForm(urlMicroservice, values)
+	if err != nil {
+		result := Result{Status: "Servidor fora do ar!"}
+		return result
+	}
+
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("Error processing result")
+	}
+
+	result := Result{}
+
+	json.Unmarshal(data, &result)
+
+	return result
 
 }
